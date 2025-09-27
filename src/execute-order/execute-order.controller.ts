8f25a560ebea184105dd22b9261b5f441cf65eb2
TophoStan <saj.tophoven@student.avans.nl>
2025-09-27 09:50:18 +0200

    feat: dynamic function execution

--- src/execute-order/execute-order.controller.ts
+++ src/execute-order/execute-order.controller.ts
@@ -1,18 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { chromium, Page } from 'playwright';
+import { ExecuteOrderDto } from './execute-order.dto';
 
 @Controller('execute-order')
 export class ExecuteOrderController {
 
-    @Get()
-    async executeOrder(): Promise<string> {
+    @Post()
+    async executeOrder(@Body() dto: ExecuteOrderDto): Promise<string> {
 
         const browser = await chromium.launch({ headless: false });
         const page = await browser.newPage();
+        console.log(dto.lines);
+        const AsyncFunction = Object.getPrototypeOf(async function () { }).constructor;
 
-        await this.AddWheyPowderToCart(page, 'Vanille');
-
-        await this.Checkout(page);
+        for (const line of dto.lines) {
+            const fn = new AsyncFunction('page', line);
+            await fn(page);
+        }
 
 
         while (true) {
@@ -22,55 +26,8 @@ export class ExecuteOrderController {
             }
         }
 
-        return page.url();
-    }
-
-    private async AddWheyPowderToCart(page: Page, flavour: string) : Promise<void> {
-
-        await page.goto('https://upfront.nl/products/whey');
-
-        await page.click('xo-toggle-trigger[xo-name="variant-flavour-toggle-main-product"]');
-        await page.waitForTimeout(2000);
-
-        await page
-            .locator('div.variant-flavour-selector__popover-inner')
-            .locator('span')
-            .filter({ hasText: 'Vanille' })
-            .first()
-            .click();
-        await page.waitForTimeout(2000);
-
-
-        await page.getByText('Voeg toe').first().click();
-
-        await page.waitForTimeout(2000);
-    }
-
-    private async Checkout(page: Page) : Promise<void> {
-        await page.getByText('Mand').first().click(); // waits until clickable
-        await page.getByText('Ik ga afrekenen').first().click();
-
-        await page.fill('input[name="email"]', 'stanthooft@gmail.com');
-        await page.fill('input[name="firstName"]', 'Stan');
-        await page.fill('input[name="lastName"]', 'Hooft');
-        await page.fill('input[name="address1"]', 'Hoveniersberg 10');
-        await page.click('#shipping-address1-options li:first-child');
-
-        await page.fill('input[name="reductions"]', 'LISAVANDERVALK');
-        var discountButton = await page.getByLabel('Pas Kortingscode Toe').first();
-
-        while (true) {
-            var res = await discountButton.getAttribute('disabled');
-            console.log(res);
-            if (!res) {
-                await page.waitForTimeout(500);
-                await discountButton.click();
-                break;
-            }
-        }
-
-        await page.fill('input[name="phone"]', '0612345678');
-        await page.getByRole('button', { name: 'Nu betalen' }).first().click();
-
+        const pageUrl = page.url();
+        await browser.close();
+        return pageUrl;
     }
 }
